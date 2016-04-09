0c686e0aabcb906ea51c147e7712c3246abdc605
Udakara Rathnayake <udakarajd@gmail.com>
2016-04-09 22:46:59 +0530

    removal

--- project_in_c/7th sem unchanged/ab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-gcc -o /home/udakarajd/Desktop/project_in_c/ga  /home/udakarajd/Desktop/project_in_c/ga.c `mysql_config --cflags --libs` -std=c99 
-chmod 777 /home/udakarajd/Desktop/project_in_c/ga
-/home/udakarajd/Desktop/project_in_c/ga
